4f3a367165ed0833dca96d791f0a5538613eef84
Mejiao99 <jonathan19991@gmail.com>
2022-03-17 14:43:09 -0500

    reformat executed

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,12 @@
-import { Container } from 'react-bootstrap'
-import utilStyles from 'styles/utils.module.css'
+import { Container } from "react-bootstrap";
+import utilStyles from "styles/utils.module.css";
 
-
-
-export default function Home({allPostsData}) {
-    return (
-        <Container>
-            hello world!
-        </Container>
-    )
+export default function Home({ allPostsData }) {
+  return <Container>hello world!</Container>;
 }
 
 export async function getStaticProps() {
-    return {
-        props: {
-        }
-    }
+  return {
+    props: {},
+  };
 }
--- src/components/AccuracyWidget.tsx
+++ src/components/AccuracyWidget.tsx
@@ -1,13 +1,11 @@
-export default function AccuracyWidget(accuracy:number) {
-    return (
-
-            <div>
-                {/* Render only with 2 digits */}
-                {(100 * accuracy).toLocaleString(undefined, {
-                    maximumFractionDigits: 1,
-                })}
-                <small className="fw-light"> %</small>
-            </div>
-
-    );
+export default function AccuracyWidget(accuracy: number) {
+  return (
+    <div>
+      {/* Render only with 2 digits */}
+      {(100 * accuracy).toLocaleString(undefined, {
+        maximumFractionDigits: 1,
+      })}
+      <small className="fw-light"> %</small>
+    </div>
+  );
 }

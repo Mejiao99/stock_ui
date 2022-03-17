9dd1f3eb907a99c72b6ad695ac5251906fb80f17
Mejiao99 <jonathan19991@gmail.com>
2022-03-17 14:02:05 -0500

    revert

--- src/components/AccuracyWidget.tsx
+++ src/components/AccuracyWidget.tsx
@@ -1,14 +1,13 @@
 export default function AccuracyWidget(accuracy:number) {
     return (
-        <div>
-            Accuracy is
+
             <div>
                 {/* Render only with 2 digits */}
                 {(100 * accuracy).toLocaleString(undefined, {
                     maximumFractionDigits: 1,
                 })}
-                %
+                <small className="fw-light"> %</small>
             </div>
-        </div>
+
     );
 }

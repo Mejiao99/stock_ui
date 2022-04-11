6823cfea282808fb7a5057a82005f73c2940c010
Jonathan M <85910530+Mejiao99@users.noreply.github.com>
2022-04-11 11:35:04 -0500

    Line chart (#42)
    * LineChart.tsx

---
+++ src/components/LineChart.tsx
@@ -0,0 +1,64 @@
+import React from "react";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Line } from "react-chartjs-2";
+import { HoldingsPerMonth } from "../../pages/sec";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+export const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: "top" as const,
+    },
+    title: {
+      display: true,
+      text: "Chart.js Line Chart",
+    },
+  },
+};
+
+interface props {
+  holdings: HoldingsPerMonth[];
+}
+
+const LineChart: React.FunctionComponent<props> = ({ holdings }) => {
+  const dataN: number[] = [];
+  const labels: string[] = [];
+
+  holdings.forEach((holding) => {
+    dataN.push(holding.holding);
+    labels.push(holding.month);
+  });
+  const data = {
+    labels,
+    datasets: [
+      {
+        label: "Port1Holdings",
+        data: dataN,
+        borderColor: "rgb(255, 99, 132)",
+        backgroundColor: "rgba(255, 99, 132, 0.5)",
+      },
+    ],
+  };
+  return <Line options={options} data={data} />;
+};
+
+export default LineChart;

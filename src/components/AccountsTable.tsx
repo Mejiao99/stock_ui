68eaa61c069dbbf0234eb7e2d22ae39abaab9f9d
Mejiao99 <jonathan19991@gmail.com>
2022-04-14 11:50:42 -0500

    text-nowrap

--- src/components/AccountsTable.tsx
+++ src/components/AccountsTable.tsx
@@ -3,7 +3,7 @@ import Table from 'react-bootstrap/Table'
 export default function AccountsTable() {
    return( <Table responsive striped bordered hover>
            <thead>
-           <tr className="bg-dark text-white-50 border border-dark ">
+           <tr className="bg-dark text-white-50 border border-dark text-nowrap">
                <th>Account</th>
                <th>TicketA</th>
                <th>TicketB</th>
@@ -14,16 +14,16 @@ export default function AccountsTable() {
            </tr>
            </thead>
            <tbody>
-           <tr className="border border-dark">
+           <tr className="border border-dark text-nowrap">
                <td>C1</td>
-               <td className="text-right">6.29 CAD</td>
-               <td className="text-right">5 USD</td>
+               <td>6.29 CAD</td>
+               <td>5 USD</td>
                <td></td>
                <td>10 CAD</td>
                <td>7.96 USD</td>
                <td>10 CAD</td>
            </tr>
-           <tr className="border border-dark">
+           <tr className="border border-dark text-nowrap">
                <td>C2</td>
                <td>12.57 CAD</td>
                <td>5 USD</td>
@@ -32,7 +32,7 @@ export default function AccountsTable() {
                <td>15.91 USD</td>
                <td>20 CAD</td>
            </tr>
-           <tr className="border border-dark">
+           <tr className="border border-dark text-nowrap">
                <td>C3</td>
                <td>6.29 CAD</td>
                <td>5 USD</td>
@@ -41,7 +41,7 @@ export default function AccountsTable() {
                <td>7.96 USD</td>
                <td>10 CAD</td>
            </tr>
-           <tr className="border border-dark">
+           <tr className="border border-dark text-nowrap">
                <td>C4</td>
                <td>6.29 CAD</td>
                <td>5 USD</td>
@@ -50,7 +50,7 @@ export default function AccountsTable() {
                <td>7.96 USD</td>
                <td>10 CAD</td>
            </tr>
-           <tr className="border border-dark">
+           <tr className="border border-dark text-nowrap">
                <td>Total</td>
                <td>31.44 CAD</td>
                <td>20 USD</td>

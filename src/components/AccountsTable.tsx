1a7f97319164ce9b2992d80e3dc7f006daea3f70
Mejiao99 <jonathan19991@gmail.com>
2022-04-20 18:13:26 -0500

    // Ticket headers

--- src/components/AccountsTable.tsx
+++ src/components/AccountsTable.tsx
@@ -100,6 +100,7 @@ function GenerateMatrix(tableResponse: GetTableResponse): Cell[][] {
       result[i][j] = StringToCell("foo");
     }
   }
+  // Ticket headers
   replaceCellsHorizontal(result, 0, 1, tableResponse.tickets.map(StringToCell))
 
   return result;

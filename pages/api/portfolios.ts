85994815f92dc094c3a8da4c5f33196f910ca191
Mejiao99 <jonathan19991@gmail.com>
2022-04-22 18:00:11 -0500

    add tablePerPortfolioDefinition add to API

--- pages/api/portfolios.ts
+++ pages/api/portfolios.ts
@@ -74,5 +74,74 @@ export default function handler(req, res) {
       cad: 1.0,
     },
     targetCurrency: "cad",
+    tablePerPortfolioDefinition: {
+      Xl1: {
+        accounts: ["C1"],
+        tickets: ["TicketA", "TicketB", "TicketC", "Currency:CAD", "Currency:USD"],
+        data: [
+          [
+            { amount: 6.29, currency: "CAD" },
+            { amount: 5, currency: "USD" },
+            { amount: 0, currency: "EUR" },
+          ],
+        ],
+        totals: {
+          accounts: [
+            { currency: "CAD", amount: 10 },
+          ],
+          tickets: [
+            { currency: "CAD", amount: 31.44 },
+            { currency: "USD", amount: 20 },
+            { currency: "EUR", amount: 4.6 },
+            { currency: "Currency:CAD", amount: 50 },
+            { currency: "Currency:USD", amount: 39.78 },
+          ],
+          total: { currency: "CAD", amount: 50 },
+        },
+      },
+      Xl2: {
+        // accounts: string[];
+        accounts: ["C1", "C2", "C3", "C4"],
+        tickets: ["TicketA", "TicketB", "TicketC", "Currency:CAD", "Currency:USD"],
+        data: [
+          [
+            { amount: 6.29, currency: "CAD" },
+            { amount: 5, currency: "USD" },
+            { amount: 0, currency: "EUR" },
+          ],
+          [
+            { amount: 12.57, currency: "CAD" },
+            { amount: 5, currency: "USD" },
+            { amount: 4.6, currency: "EUR" },
+          ],
+          [
+            { amount: 6.29, currency: "CAD" },
+            { amount: 5, currency: "USD" },
+            { amount: 0, currency: "EUR" },
+          ],
+          [
+            { amount: 6.29, currency: "CAD" },
+            { amount: 5, currency: "USD" },
+            { amount: 0, currency: "EUR" },
+          ],
+        ],
+        totals: {
+          accounts: [
+            { currency: "CAD", amount: 10 },
+            { currency: "USD", amount: 20 },
+            { currency: "CAD", amount: 10 },
+            { currency: "CAD", amount: 10 },
+          ],
+          tickets: [
+            { currency: "CAD", amount: 31.44 },
+            { currency: "USD", amount: 20 },
+            { currency: "EUR", amount: 4.6 },
+            { currency: "Currency:CAD", amount: 50 },
+            { currency: "Currency:USD", amount: 39.78 },
+          ],
+          total: { currency: "CAD", amount: 50 },
+        },
+      },
+    },
   });
 }

cdae8491cd82a167ed11e12a587ab2fbaba8849e
Mejiao99 <jonathan19991@gmail.com>
2022-06-05 13:39:29 -0500

    market

--- pages/market/index.tsx
+++ pages/market/index.tsx
@@ -1,12 +1,36 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
+import { Card } from "react-bootstrap";
 
-export default function Home({staticProps}) {
-    const [state, setState] = useState("hello");
-    function clicked() {
-        fetch(staticProps.backendHost + "/market")
-            .then((received) => received.json())
-            .then((data) => data["market"] as string)
-            .then((receivedState) => setState(receivedState));
-    }
-    return  <button onClick={clicked}>{state}</button>;
+function SimpleCard({ state }) {
+  return (
+    <Card className="m-3 ">
+      <Card.Header className="text-center">{state}</Card.Header>
+    </Card>
+  );
+}
+
+export default function Home(props) {
+  const [state, setState] = useState("hello");
+
+  function clicked() {
+    fetch(props.backendHost + "/market")
+      .then((received) => received.json())
+      .then((data) => data["data"] as string)
+      .then((receivedState) => setState(receivedState));
+  }
+
+  return (
+    <>
+      <button onClick={clicked}>Submit</button>
+      <SimpleCard state={state} />
+    </>
+  );
+}
+
+export async function getStaticProps() {
+  return {
+    props: {
+      backendHost: process.env.BACKEND_HOST,
+    },
+  };
 }
